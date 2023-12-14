06d51e5d5120b03b096a9ffacdd0f53e38a21fa8
Emmanuel Orestes Torres <65071460+eotorres@users.noreply.github.com>
2023-12-14 10:26:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("<h1 style='text-align: center;'>📈 Previsão automatizada de sér
 	        ,unsafe_allow_html=True)
 
 """
-Este aplicativo de dados usa a biblioteca de código aberto do Facebook, Prophet, para gerar automaticamente valores de previsão futura a partir de um conjunto de dados importado.
+Este aplicativo de dados usa a biblioteca de código aberto do Facebook Prophet, para gerar automaticamente valores de previsão futura a partir de um conjunto de dados importado.
 Você poderá importar seus dados de um arquivo CSV e o separador precisa ser por ';', visualizar tendências e recursos, analisar o desempenho da previsão e, finalmente, baixar a previsão criada 😵
 """
 
