384453f751f91f2728cd627f9b18374e57202b3e
Emmanuel Orestes Torres <65071460+eotorres@users.noreply.github.com>
2024-02-28 12:08:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.markdown("<h1 style='text-align: center;'>📈 Previsão automatizada de sér
 
 """
 Este aplicativo de dados usa a biblioteca de código aberto do Facebook Prophet, para gerar automaticamente valores de previsão futura a partir de um conjunto de dados importado.
-Você poderá importar seus dados de um arquivo CSV e o separador precisa ser por ',', visualizar tendências e recursos, analisar o desempenho da previsão e, finalmente, baixar a previsão criada 😵
+Você poderá importar seus dados de um arquivo CSV e o separador precisa ser por ';', visualizar tendências e recursos, analisar o desempenho da previsão e, finalmente, baixar a previsão criada 😵
 """
 
 """
@@ -31,7 +31,7 @@ st.info(
                 """
         )
 if df is not None:
-    data = pd.read_csv(df,sep=',')
+    data = pd.read_csv(df,sep=';')
     data['ds'] = pd.to_datetime(data['ds'],errors='coerce')     
     st.write(data)    
     max_date = data['ds'].max()    
